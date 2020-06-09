c5a32b5828f24a32f01f997579799ede722c0070
ComplexPlane <complexplanegames@gmail.com>
2020-06-09 19:09:22 -0400

    Rename to pausetimeout

--- inject.sh
+++ inject.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cp "$DIR"/../smb2imm/sys/main.dol "$DIR"/../smb2mut/sys
-"$DIR"/../util/ppc-inject/PPCInject "$DIR"/../smb2imm/files/mkb2.main_loop.rel "$DIR"/../smb2mut/files/mkb2.main_loop.rel "$DIR"/iw.asm
+"$DIR"/../util/ppc-inject/PPCInject "$DIR"/../smb2imm/files/mkb2.main_loop.rel "$DIR"/../smb2mut/files/mkb2.main_loop.rel "$DIR"/pausetimeout.asm
 # "$DIR"/../util/ppc-inject/PPCInject "$DIR"/../smb2imm/files/mkb2.main_game.rel "$DIR"/../smb2mut/files/mkb2.main_game.rel "$DIR"/main_game.asm
